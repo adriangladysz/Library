d1167a67cad5fcb25a7dc0344b7002b612657304
adriangladysz <adrian.gladysz07@gmail,com>
2022-02-03 16:00:13 +0100

    Library v0.2

---
+++ src/Library.java
@@ -0,0 +1,22 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName="Biblioteka v0.2";
+
+        Book book1=new Book();
+        book1.title="W pustyni i w puszczy";
+        book1.author="Henryk Sienkiewicz";
+        book1.releaseDate=2010;
+        book1.pages=296;
+        book1.publisher="Greg";
+        book1.isbn="123461278034";
+
+        System.out.println(appName);
+        System.out.println("Ksiązki dostepne w bibliotece");
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
+    }
+}
